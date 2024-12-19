413bcccfff62869b75fb8ab8733b08e6d90742a3
Thierry <dev@coworkingcafe.fr>
2024-12-19 11:59:22 +0100

    JB

--- src/App.jsx
+++ src/App.jsx
@@ -15,32 +15,77 @@ import SupCounter from "./components/SupCounter";
 import { useState } from "react";
 
 function App() {
-  const [array, setArray] = useState([<Counter />]);
+  /** V2 */
+  // const [array, setArray] = useState([<Counter />]);
+  // // const addCounter = () => {
+  // //   setArray([...array, <Counter />]);
+  // // };
   // const addCounter = () => {
-  //   setArray([...array, <Counter />]);
+  //   const newArray = [...array];
+  //   newArray.push(<Counter />);
+  //   setArray(newArray);
   // };
-  const addCounter = () => {
-    const newArray = [...array];
-    newArray.push(<Counter />);
-    setArray(newArray);
-  };
-  const supCounter = () => {
-    const newArray = [...array];
-    newArray.pop(<Counter />);
-    setArray(newArray);
-  };
+  // const supCounter = () => {
+  //   const newArray = [...array];
+  //   newArray.pop(<Counter />);
+  //   setArray(newArray);
+  // };
+  // console.log(array);
 
-  console.log(array);
+  /** V3 */
+  // Prepare the table of all values.
+  const [counterValues, setCounterValues] = useState([0]);
+
+  // Callback used to add a new counter.
+  function addCounter() {
+    // This is the same as:
+    // const newValues = [...counterValues];
+    // newValues.push(0);
+    // setCounterValues(newValues);
+    setCounterValues(counterValues.concat(0));
+  }
+
+  // Callback used to remove the last counter.
+  function removeCounter() {
+    // This is the same as:
+    // const newValues = [...counterValues];
+    // newValues.pop();
+    // setCounterValues(newValues);
+    setCounterValues(counterValues.slice(0, counterValues.length - 1));
+  }
+
+  // Callback used to update a value in the counter.
+  function setCounterValue(value, index) {
+    // This is the same as:
+    // const newValues = [...counterValues];
+    // newValues[index] = value;
+    // setCounterValues(newValues);
+    setCounterValues(counterValues.toSpliced(index, 1, value));
+  }
+
+  // Render all the counters.
+  const counterElements = counterValues.map(function renderCounterValue(
+    value,
+    index
+  ) {
+    return (
+      <Counter
+        key={index}
+        value={value}
+        updateCounterValue={(value) => setCounterValue(value, index)}
+      />
+    );
+  });
 
   return (
     <>
       <Header />
       <main>
         <div className="add-counter">
-          {array.length < 3 && <AddCounter addCounter={addCounter} />}
+          {counterValues.length < 3 && <AddCounter addCounter={addCounter} />}
         </div>
-        <section>{array}</section>
-        {array.length > 1 && <SupCounter supCounter={supCounter} />}
+        <section>{counterElements}</section>
+        {counterValues.length > 1 && <SupCounter supCounter={removeCounter} />}
       </main>
     </>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,31 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
-const Counter = () => {
-  const [counter, setCounter] = useState(0);
+const Counter = ({ value, updateCounterValue }) => {
+  // const [counter, setCounter] = useState(0);
   const decrement = () => {
-    setCounter(counter - 1);
+    updateCounterValue(value - 1);
   };
   const increment = () => {
-    setCounter(counter + 1);
+    updateCounterValue(value + 1);
   };
   const reset = () => {
-    setCounter(0);
+    updateCounterValue(0);
   };
 
   return (
     <div className="component">
       <div className="container">
         <div className="box-icon">
-          {counter > 0 && (
+          {value > 0 && (
             <button onClick={decrement}>
               <FontAwesomeIcon icon="minus" />
             </button>
           )}
         </div>
-        <div className="counter">{counter}</div>
+        <div className="counter">{value}</div>
         <div className="box-icon">
-          {counter < 10 && (
+          {value < 10 && (
             <button onClick={increment}>
               <FontAwesomeIcon icon="plus" />
             </button>
